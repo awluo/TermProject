466f76f887f58a9f93911aa5f097b7c600be1806
luoa2945 <AmyLuo1999@gmail.com>
2018-12-04 15:44:42 -0500

    Add files via upload

--- JUMPYGAME.py
+++ JUMPYGAME.py
@@ -11,7 +11,7 @@ class Game:
         pg.init()
         self.font = pg.font.SysFont('arial bold', 40)
         
-        #frames per second
+        #frames per sec
         self.FPS = 60
         
         #graphics dimenions: width and height
@@ -29,11 +29,14 @@ class Game:
         self.platH = 15 #platform height
         self.cloud = pg.image.load('cloud.png') #platform images
         self.cloud2 = pg.image.load('cloud2.png')
+        self.darkCloud = pg.image.load('darkCloud.png')
         self.coin = pg.image.load('coin.png') #coin image
         self.bee = pg.image.load('bee.png') #enemy image
         self.heart = pg.image.load('heart.png') #heart image
+        self.rainDrop = pg.image.load('rainDrop.png')
+        self.singlePlayer = True
     
-    def new(self):
+    def new(self, playerMode = True):
         #start a new game
         self.allSprites = pg.sprite.Group()
         self.platforms = pg.sprite.Group()
@@ -41,8 +44,13 @@ class Game:
         self.bullets = pg.sprite.Group()
         self.enemyBullets = pg.sprite.Group()
         self.enemies = pg.sprite.Group()
-        self.player = Player(self)
+        self.players = pg.sprite.Group()
+        #self.player = Player(self)
         self.lives = 3
+        self.level = 1
+        self.distance = 0
+        self.mode = "start"
+        self.singlePlayer = playerMode
         
         self.newHighScore = False
         #starting platforms
@@ -57,15 +65,22 @@ class Game:
         self.score = 0
         self.mobTimer = 0
         self.run()
-    
+        
     def run(self):
         # Game Loop
         self.playing = True
         while self.playing:
-            self.clock.tick(FPS)
-            self.events()
-            self.update()
-            self.draw()
+            if self.mode == "game":
+                self.clock.tick(FPS)
+                self.events()
+                self.update()
+                self.drawGame()
+            elif self.mode == "start":
+                self.showStartScreen()
+            elif self.mode == "instructions":
+                self.showInstructions()
+            elif self.mode == "settings":
+                self.showSettings()
         
     def update(self):
         #Game Loop - Update
@@ -73,31 +88,18 @@ class Game:
         
         #spawn enemy
         now = pg.time.get_ticks()
-        if now - self.mobTimer > 5000 + random.choice([-1000, -500, 0, 500, 1000]):
+        if now - self.mobTimer > 10000 - self.level/2*1000 + \
+        random.choice([-1000, -500, 0, 500, 1000]):
             self.mobTimer = now
             Enemy(self)
-        # hit mobs?
+        #check collision with enemies/enemy bullets
         mobHits = pg.sprite.spritecollide(self.player, self.enemies, True)
         enemyHits = pg.sprite.spritecollide(self.player, self.enemyBullets, True)
         if mobHits or enemyHits:
-            self.loseLife()
+            self.loseLife() #lose a life
         
         #check platform collisions only if falling
-        if self.player.vel.y > 0:
-            hits = pg.sprite.spritecollide(self.player, self.platforms, False)
-            if hits:
-                lowest = hits[0]
-                #find lowest platform hit
-                for hit in hits:
-                    if hit.rect.bottom > lowest.rect.bottom:
-                        lowest = hit
-                if self.player.pos.x < lowest.rect.right + 10 and \
-                    self.player.pos.x > lowest.rect.left - 10:
-                    #land on object
-                    if self.player.pos.y < lowest.rect.centery:
-                        self.player.pos.y = lowest.rect.top
-                        self.player.vel.y = 0 #stop moving downward
-                        self.player.jumping = False
+        self.checkPlatCollide()
         
         #check if player hits power  up
         self.checkPowerUps()
@@ -125,8 +127,36 @@ class Game:
             for hit in enemyHits:
                 self.score += 10
 
+    #check platform collision if player is falling
+    def checkPlatCollide(self):
+        self.checkPlatCollideHelper(self.player)
+        if not self.singlePlayer:
+            self.checkPlatCollideHelper(self.player2)
+    
+    def checkPlatCollideHelper(self, player):   
+        if player.vel.y > 0:
+            hits = pg.sprite.spritecollide(player, self.platforms, False)
+            if hits:
+                lowest = hits[0]
+                #find lowest platform hit
+                for hit in hits:
+                    if hit.rect.bottom > lowest.rect.bottom:
+                        lowest = hit
+                    if hit.type == "dark":
+                        #hit.kill() #delete dark cloud
+                        hit.jumpedOn = True
+                        hit.jumpTime = pg.time.get_ticks()
+                if player.pos.x < lowest.rect.right + 10 and \
+                    player.pos.x > lowest.rect.left - 10:
+                    #land on object
+                    if player.pos.y < lowest.rect.centery:
+                        player.pos.y = lowest.rect.top
+                        player.vel.y = 0 #stop moving downward
+                        player.jumping = False
+
+    #lose a life
     def loseLife(self):
-        self.lives -= 1 #lose a life
+        self.lives -= 1 
         if self.lives == 0: self.playing = False
         
     def events(self):
@@ -140,25 +170,52 @@ class Game:
             elif event.type == pg.KEYDOWN:
                 if event.key == pg.K_UP:
                     self.player.jump()
+                elif not self.singlePlayer and event.key == pg.K_w:
+                    self.player2.jump()
             elif event.type == pg.KEYUP:
                 if event.key == pg.K_UP:
                     self.player.jumpCut()
                     #space to shoot
+                elif not self.singlePlayer and event.key == pg.K_w:
+                    self.player2.jumpCut()
                 if event.key == pg.K_SPACE:
                     self.player.shoot()
+                elif not self.singlePlayer and event.key == pg.K_LCTRL:
+                    self.player2.shoot()
+                
         
         #key presses            
         keys = pg.key.get_pressed()       
         if keys[pg.K_RIGHT]:
             self.player.dir = 1
-            self.movePlatforms()
+            self.updateDistance()
         elif  keys[pg.K_LEFT]:
             self.player.dir = -1
-            self.movePlatforms()
+            self.updateDistance()
+        if not self.singlePlayer:
+            if keys[pg.K_d]:
+                self.player2.dir = 1
+            elif  keys[pg.K_a]:
+                self.player2.dir = -1
                 
         #spawn new platforms
         while len(self.platforms) < 12:
             self.newPlatform()
+    
+    #update player distance, move platforms, check level up
+    def updateDistance(self):
+        #move platforms
+        self.movePlatforms()
+        #increase distance
+        self.distance += int(self.player.vel.x)
+        #check level up
+        if self.distance >= 1000:
+            self.updateLevel()
+            self.distance = 0 #reset to new level
+            
+    #go to next level
+    def updateLevel(self):
+        self.level += 1
     
     #move platforms in opposite direction
     def movePlatforms(self):
@@ -177,7 +234,7 @@ class Game:
         Platform(self, x, y) #spawn new platform
         
     #draw game screen
-    def draw(self):
+    def drawGame(self):
         #Game Loop - draw
         self.screen.fill((50,150,250))
     
@@ -185,11 +242,25 @@ class Game:
         #draw player over everything in front
         self.screen.blit(self.player.image, self.player.rect)
         
+        if not self.singlePlayer:
+            self.drawText("p1", 25, 
+            RED, self.player.rect.midtop[0], self.player.rect.midtop[1] - 30)
+            self.drawText("p2", 25, 
+            BLUE, self.player2.rect.midtop[0], self.player2.rect.midtop[1] - 30)
+            
+        
         #draw hearts
         self.drawLives()
         
         self.drawText("Score: " + str(self.score), 
         40, (250,250,250), WIDTH/2, 15)
+        
+        self.drawText("Level: " + str(self.level), 
+        40, (250,250,0), 80, 15)
+        
+        self.drawText("Dist: " + str(self.distance), 
+        40, (250,250,0), 80, 50)
+        
         pg.display.flip()
     
     #draw player lives
@@ -203,11 +274,19 @@ class Game:
         #game start screen
         self.screen.fill((0,0,0))
         self.drawText("JUMP CAT", 50, (200,200,0), WIDTH/2, HEIGHT/4)
-        self.drawText("Arrows to Move, Space to Shoot", 30, 
-        (200,200,200), WIDTH/2, HEIGHT/2)
 
         self.drawText("[Press a key to play]", 30, 
         WHITE, WIDTH / 2, HEIGHT * 3 / 4)
+        
+        self.buttonCol = (0,200,250)
+        self.instructButton = Button(self, self.buttonCol, self.screenW//2, 
+        self.screenH//2, 200, 50, 'Instructions')
+        self.instructButton.draw(self.screen)
+        
+        self.settingsButton = Button(self, self.buttonCol, self.screenW//2, 
+        self.screenH//1.5, 200, 50, 'Settings')
+        self.settingsButton.draw(self.screen)
+            
         pg.display.flip()
         self.wait_for_key()
     
@@ -240,6 +319,51 @@ class Game:
             yIndex += 1
             self.drawText(str(score), 25,
             WHITE, WIDTH / 2, HEIGHT/2.5 + yIndex*30)
+            
+    def showInstructions(self):
+        self.screen.fill((0,0,0))
+        margin = 80
+        self.drawText("* ~ Arrow Keys to move, Space to shoot", 30, 
+                (0,200,200), WIDTH/2, HEIGHT/4)
+
+        self.drawText("* ~ Dodge Enemy Bees and Rain Clouds", 30, 
+                (0,200,200), WIDTH/2, HEIGHT/4 + margin)
+
+        self.drawText("* ~ Collect Extra Lives and Coins to earn points", 30, 
+                (0,200,200), WIDTH/2, HEIGHT/4 + margin*2)
+
+        self.drawText("[Press a key to return]", 30, 
+                (200,200,200), WIDTH/2, HEIGHT/4 + margin*3)
+        pg.display.flip()
+
+        self.wait_for_key()
+    
+    def showSettings(self):
+        self.screen.fill((0,0,0))
+        margin = 80
+        
+        self.buttonCol, self.buttonCol2 = (0,200,250), (0,200,250)
+        if self.singlePlayer: self.buttonCol = (250,250,250)
+        else: self.buttonCol2 = (250, 250, 250)
+        self.singlePlayerButton = Button(self, self.buttonCol, self.screenW//2, 
+        self.screenH//4, 200, 50, 'Single Player')
+        self.singlePlayerButton.draw(self.screen)
+        
+        self.twoPlayerButton = Button(self, self.buttonCol2, self.screenW//2, 
+        self.screenH//4 + margin, 200, 50, 'Two Player')
+        self.twoPlayerButton.draw(self.screen)
+
+        '''self.drawText("Level Editor", 30, 
+                (200,200,200), WIDTH/2, HEIGHT/4 + margin*2)
+
+        self.drawText("Character Editor", 30, 
+                (200,200,200), WIDTH/2, HEIGHT/4 + margin*3)'''
+                
+        self.drawText("[Press a key to return]", 30, 
+                (200,200,200), WIDTH/2, HEIGHT/4 + margin*3)
+        pg.display.flip()
+
+        self.wait_for_key()
     
     #wait for key press
     def wait_for_key(self):
@@ -247,12 +371,41 @@ class Game:
         while waiting:
             self.clock.tick(FPS)
             for event in pg.event.get():
+                mousePos = pg.mouse.get_pos()
                 if event.type == pg.QUIT:
                     waiting = False
+                    self.playing = False
                     self.running = False
+                elif self.mode == "start":
+                    if self.instructButton.isOver(mousePos):
+                        if event.type == pg.MOUSEBUTTONDOWN:
+                            waiting = False
+                            self.mode = "instructions"
+                    elif self.settingsButton.isOver(mousePos):
+                        if event.type == pg.MOUSEBUTTONDOWN:
+                            waiting = False
+                            self.mode = "settings"
+                elif self.mode == "settings":
+                    if self.singlePlayerButton.isOver(mousePos):
+                        if event.type == pg.MOUSEBUTTONDOWN:
+                            waiting = False
+                            self.singlePlayer = True
+                    elif self.twoPlayerButton.isOver(mousePos):
+                        if event.type == pg.MOUSEBUTTONDOWN:
+                            waiting = False
+                            self.singlePlayer = False
                 if event.type == pg.KEYDOWN:
                     waiting = False
-        
+                    if self.mode == "start": 
+                        self.startGame()
+                    else: self.mode = "start"
+                    
+    def startGame(self):
+        self.mode = "game"
+        self.player = Player(self)
+        if not self.singlePlayer:
+            self.player2 = Player2(self)
+            
     #draw text on screen
     def drawText(self, text, size, color, x, y):
         font = pg.font.SysFont('arial bold', size)
@@ -272,14 +425,14 @@ class Game:
                 self.newHighScore = True
                 break
     
+    #animate frames
     def animate(self):
         now = pg.time.get_ticks()
     
 #Run game!
 g = Game()
-g.showStartScreen()
 while g.running:
-    g.new()
+    g.new(g.singlePlayer)
     g.showGameOver()
 pg.quit()
                 
--- sprites.py
+++ sprites.py
@@ -6,7 +6,7 @@ import random
 
 class Player(pg.sprite.Sprite):
     def __init__(self, game):
-        self.groups = game.allSprites
+        self.groups = game.allSprites, game.players
         #add sprite to these groups
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game #game info
@@ -110,6 +110,8 @@ class Player(pg.sprite.Sprite):
         if self.jumping:
             bottom = self.rect.bottom
             self.image = self.jumpImage
+            if self.dir == -1:
+                self.image = pg.transform.flip(self.jumpImage, True, False)
             self.rect = self.image.get_rect()
             self.rect.bottom = bottom
         elif self.walking:
@@ -131,7 +133,63 @@ class Player(pg.sprite.Sprite):
     #shoot bullets        
     def shoot(self):
         Bullet(self.game, self.rect.centerx, self.rect.centery, self.dir)
+
+class Player2(Player):
+    def __init__(self, game):
+        self.groups = game.allSprites, game.players
+        #add sprite to these groups
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game #game info
+        self.walking = False
+        self.jumping = False
+        self.currFrame = 0
+        self.lastUpdate = 0 #frame rate of walking animation
+        self.loadImages() #load image files
         
+        self.image = self.standingImage #standing at start
+        self.rect = self.image.get_rect()
+        
+        scale = 3
+        size = self.image.get_size()
+        self.w, self.h = size[0], size[1]
+            
+        self.pos = vec(WIDTH/2 + 50, HEIGHT/2)
+        self.vel = vec(0,0) #vec(xVel, yVel)
+        self.acc = vec(0,0) #vec(xAcc, yA)
+        self.jumpCount = 0
+        self.dir = 1 #facing direction right
+            
+        self.rect.center = (WIDTH/2, 0)
+
+    def update(self):
+        self.animate() #get next image frame
+        
+        self.acc = vec(0, PLAYER_GRAV)
+        #manage key presses
+        keys = pg.key.get_pressed()
+        if keys[pg.K_a]:
+            self.acc.x = -PLAYER_ACC
+        if keys[pg.K_d]:
+            self.acc.x = PLAYER_ACC
+
+        # apply friction
+        self.acc.x += self.vel.x * PLAYER_FRICTION
+        # equations of motion
+        self.vel += self.acc
+        self.pos.x += self.vel.x
+        self.pos.x -= int(self.game.player.vel.x)
+        #if not moving
+        if abs(self.vel.x) < 0.1:
+            self.vel.x = 0
+            
+        self.pos.y += min(15, self.vel.y + 0.5 * self.acc.y)
+        # wrap around the sides of the screen
+        if self.pos.x > WIDTH:
+            self.pos.x -= self.vel.x
+        if self.pos.x < 0:
+            self.pos.x -= self.vel.x
+
+        self.rect.midbottom = self.pos
                     
 #platforms to jump on            
 from random import choice, randrange
@@ -141,18 +199,47 @@ class Platform(pg.sprite.Sprite):
         #add sprite to these groups
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self.images = [self.game.cloud, self.game.cloud2]
-        self.image = choice(self.images)
+        self.type = "normal"
+        
+        #select type of cloud
+        if randrange(100) < 1 + game.level:
+            #dark cloud
+            self.type = "dark"
+            self.image = self.game.darkCloud
+            self.jumpedOn = False
+            self.jumpTime = 0
+        else:
+            #normal cloud
+            self.images = [self.game.cloud, self.game.cloud2]
+            self.image = choice(self.images)
+        
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.lastBullet = 0
+        
         #likelihood of getting power up
         if randrange(100) < 10:
             #generate power up on platform
             PowerUp(self.game, self)
-        elif randrange(100) < 10:
+        elif randrange(100) < 5:
             #generate extra life
             ExtraLife(self.game, self)
+    
+    def update(self):
+        if self.type == "dark":
+            now = pg.time.get_ticks()
+            #fire bullet
+            if self.jumpedOn and now - self.jumpTime > 200:
+                self.kill() #delete cloud
+            
+            elif now - self.lastBullet > 2000 + random.choice([-1000, 
+            -500, 0, 500, 1000]):
+                self.lastBullet = now
+                EnemyBullet(self.game, self.rect.centerx, 
+                self.rect.bottom, 0,1, True)
+                
+            
   
 #powerUp on platform
 class PowerUp(pg.sprite.Sprite):
@@ -198,7 +285,7 @@ class ExtraLife(pg.sprite.Sprite):
         self.rect.centerx = self.plat.rect.centerx
         #check if platform still exists
         if not self.game.platforms.has(self.plat):
-            self.kill() #delete powerup'''
+            self.kill() #delete powerup
         
 
 #enemy sprites to attack player
@@ -226,8 +313,7 @@ class Enemy(pg.sprite.Sprite):
         -500, 0, 500, 1000]):
             self.lastBullet = now
             EnemyBullet(self.game, self.rect.centerx, self.rect.centery, 
-            self.vx/abs(self.vx))
-        
+            self.vx/abs(self.vx), 0, False)
         self.rect.x += self.vx
         self.vy += self.dy
         if self.vy > 3 or self.vy < -3:
@@ -240,6 +326,7 @@ class Enemy(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = center
         self.rect.y += self.vy
+        #self.rect.x -= self.game.player.vel.x
         #moved off of the screen
         if self.rect.left > WIDTH + 100 or self.rect.right < -100:
             self.kill()
@@ -259,26 +346,67 @@ class Bullet(pg.sprite.Sprite):
         self.rect.centery = y
         self.rect.centerx = x
         self.xSpeed = 10 * dir
+        self.ySpeed = 0
         
     def update(self):
-        self.rect.x += self.xSpeed
+        self.rect.x += self.xSpeed - self.game.player.vel.x
+        self.rect.y += self.ySpeed
         #kill if it moves off the screen
-        if self.rect.x < 0 or self.rect.x > self.game.screenW:
+        if self.rect.x < 0 or self.rect.x > self.game.screenW or \
+            self.rect.y < 0 or self.rect.y > self.game.screenH:
             self.kill()
         
 #bullets to attack and fire
 class EnemyBullet(Bullet):
-    def __init__(self, game, x, y, dir):
+    def __init__(self, game, x, y, xVel, yVel, isRain):
         self.groups = game.allSprites, game.enemyBullets
         #add sprite to these groups
         pg.sprite.Sprite.__init__(self, self.groups)
         #bullet attributes
-        self.image = pg.Surface((10, 10))
-        self.image.fill((250, 10, 10))
+        if isRain:
+            self.image = game.rainDrop
+        else:
+            self.image = pg.Surface((10, 10))
+            self.image.fill((250, 10, 10))
         self.rect = self.image.get_rect()
         self.game = game
         self.rect.centery = y
         self.rect.centerx = x
-        self.xSpeed = 10 * dir
-
+        self.xSpeed = 10 * xVel
+        self.ySpeed = 10 * yVel
+        
+class Button(pg.sprite.Sprite):
+    def __init__(self, game, color, x, y, w, h, text = ''):
+        self.color = color
+        self.image = pg.Surface((w, h))
+        self.image.fill((200, 200, 0))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.centery = y
+        self.x, self.y = x - w/2, y - h/2 #top left corner
+        self.w, self.h = w, h
+        self.text = text
+        self.game = game
+    
+    #draw button on the screen
+    def draw(self, win):
+        pg.draw.rect(win, self.color, self.rect, 0)
+        if self.text != '':
+            self.game.drawText(self.text, int(self.h//1.5), 
+            (0,0,0), self.rect.centerx, self.rect.centery - self.h/5)
+    
+    #check if mouse is hovering over button
+    def isOver(self, pos):
+        #mousePos is tuple of mouse coordinates
+        mouseX, mouseY = pos[0], pos[1]
+        if mouseX > self.x and mouseX < self.x + self.w and \
+            mouseY > self.y and mouseY < self.y + self.h:
+                return True
+        return False
+            
+            
+            
+            
+            
+            
 
